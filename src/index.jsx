204a2073184f640c171f501e1865855c4d594b14
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-03-26 16:07:34 -0300

    added redux-promise middleware

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger'
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
@@ -30,7 +31,7 @@ const reducers = combineReducers({
   currentUser: identityReducer
 });
 
-const middlewares = applyMiddleware(logger);
+const middlewares = applyMiddleware(logger, reduxPromise);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
